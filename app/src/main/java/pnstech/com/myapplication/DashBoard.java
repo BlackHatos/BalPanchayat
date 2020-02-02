3a33ac38caf4e2e37f3fd9e68ce9a42462492ff2
Shubham Maurya <pnssoftwares7@gmail.com>
2020-02-02 18:42:43 +0530

    connection status applied

--- app/src/main/java/pnstech/com/myapplication/DashBoard.java
+++ app/src/main/java/pnstech/com/myapplication/DashBoard.java
@@ -1,14 +1,19 @@
 package pnstech.com.myapplication;
 
 import android.app.Notification;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.annotation.NonNull;
 import android.support.design.internal.BottomNavigationItemView;
 import android.support.design.internal.BottomNavigationMenuView;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
@@ -45,6 +50,7 @@ public class DashBoard extends AppCompatActivity {
     private ImageView user_image;
     private SharedPreferences sharedPreferences;
     private Button leave_admin;
+    private RelativeLayout relativeLayout;
 
     private BottomNavigationView bottomNavigationView;
     private View notificationBadge;
@@ -54,6 +60,9 @@ public class DashBoard extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+
+        relativeLayout = findViewById(R.id.relative_layout);
+
         user_name = (TextView) findViewById(R.id.user_name);
         user_image = (ImageView) findViewById(R.id.user_image);
 
@@ -196,7 +205,7 @@ public class DashBoard extends AppCompatActivity {
     }
 
 
-    //mthod to visit to profile
+    //method to visit to profile
     public void goToProfile(View view){
         startActivity(new Intent(DashBoard.this,Profile.class));
     }
@@ -204,7 +213,21 @@ public class DashBoard extends AppCompatActivity {
     //method to go to library
 
     public void goToLibrary(View view){
+        boolean isConnected  = checkInternetConnection();
+        if(isConnected)
         startActivity(new Intent(DashBoard.this,MainLibrary.class));
+        else
+        {
+            final Snackbar snackbar = Snackbar.make(relativeLayout,"You need an active internet connection",Snackbar.LENGTH_INDEFINITE);
+            snackbar.setActionTextColor(Color.YELLOW);
+            snackbar.setAction("OK", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    snackbar.dismiss();
+                }
+            });
+            snackbar.show();
+        }
     }
 
 
@@ -213,27 +236,75 @@ public class DashBoard extends AppCompatActivity {
     public void goToAdmin(View view)
     {
         String code = sharedPreferences.getString("userType","");
-        if(code.equals("1"))
+
+        boolean isConnected  = checkInternetConnection();
+        if(isConnected)
         {
-            //go to upload image
-            startActivity(new Intent(DashBoard.this, SendNotification.class));
+            if(code.equals("1"))
+            {
+                //go to send notification
+                startActivity(new Intent(DashBoard.this, SendNotification.class));
+            }
+            else
+            {
+                notAllowedPopup(view);
+            }
         }
         else
         {
-            notAllowedPopup(view);
+            final Snackbar snackbar = Snackbar.make(relativeLayout,"You need an active internet connection",Snackbar.LENGTH_INDEFINITE);
+            snackbar.setActionTextColor(Color.YELLOW);
+            snackbar.setAction("OK", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    snackbar.dismiss();
+                }
+            });
+            snackbar.show();
         }
+
     }
 
     public void goToDonate(View view)
     {
-        startActivity(new Intent(DashBoard.this,Donars.class));
+
+        boolean isConnected  = checkInternetConnection();
+        if(isConnected)
+            startActivity(new Intent(DashBoard.this,Donars.class));
+        else
+        {
+            final Snackbar snackbar = Snackbar.make(relativeLayout,"You need an active internet connection",Snackbar.LENGTH_INDEFINITE);
+            snackbar.setActionTextColor(Color.YELLOW);
+            snackbar.setAction("OK", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    snackbar.dismiss();
+                }
+            });
+            snackbar.show();
+        }
+
     }
 
     public void goToTeam(View view)
     {
-        startActivity(new Intent(DashBoard.this, Team.class));
-    }
+        boolean isConnected  = checkInternetConnection();
+        if(isConnected)
+            startActivity(new Intent(DashBoard.this, Team.class));
+        else
+        {
+            final Snackbar snackbar = Snackbar.make(relativeLayout,"You need an active internet connection",Snackbar.LENGTH_INDEFINITE);
+            snackbar.setActionTextColor(Color.YELLOW);
+            snackbar.setAction("OK", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    snackbar.dismiss();
+                }
+            });
+            snackbar.show();
+        }
 
+    }
 
     public void   notAllowedPopup(View view)
     {
@@ -255,6 +326,13 @@ public class DashBoard extends AppCompatActivity {
             }
         });
 
+    }
+
+    public boolean checkInternetConnection()
+    {
+        ConnectivityManager connectivityManager  = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo=  connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 
 }
